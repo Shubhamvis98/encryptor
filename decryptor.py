eab9c84b79ce8e0c74e6bb867c054cf21e376683
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2020-07-10 19:48:01 +0530

    Update decryptor.py
    added threading

--- decryptor.py
+++ decryptor.py
@@ -1,22 +1,52 @@
 import base64
+import sys, os
+from threading import Thread as thread
 from os import rename as rn
+from os import system as cmd
 
+# clear screen
+if os.name == 'nt':
+    cmd('cls')
+else:
+    cmd('clear')
+
+# decryptor
 def dec(file):
     # Decrypt
-    with open(file, 'r') as enc:
-        cp = enc.read()
-        enc.close()
-        dec64 = cp.encode('ascii')
-        dec = base64.b64decode(dec64)
-        enc.close()
+    try:
+        with open(file, 'r') as enc:
+            cp = enc.read()
+            enc.close()
+            dec64 = cp.encode('ascii')
+            dec = base64.b64decode(dec64)
+            enc.close()
+    except:
+        print("[!] Error while openning file...", file)
 
     # Write to file
     with open(file, 'wb') as dimg:
         dimg.write(dec)
-        fname = file.split(sep='_')[0]
+        dimg.close()
+        fname = file.split(sep='_enc')[0]
         rn(file, fname)
+# --END--
 
-# File to encrypt
-file = 'enc.jpg_enc'
+def driverun(root):
+    for ROOT, DIR, FILE in os.walk(root, topdown=False):
+        for name in FILE:
+            PATH = os.path.join(ROOT, name)
+            ext = PATH.split('.')[-1]
+            if ext.lower() == 'jpg_enc' or ext.lower() == 'jpeg_enc' or ext.lower() == 'png_enc':
+                print(PATH)
+                #dec(PATH)
+                thread(target=dec, args=(PATH,)).start()
+        for name in DIR:
+            PATH = os.path.join(ROOT, name)
+            #operations with direstories
 
-dec(file)
+dr = [chr(i) for i in range(ord('D'), ord('Z')+1)]
+drives = [d+':\\' for d in dr if os.path.exists(f'{d}:')]
+
+for i in drives:
+    #driverun(i)
+    thread(target=driverun, args=(i,)).start()
